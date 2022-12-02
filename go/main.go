00bb50db1beb34e70e92f26f35a6a570cae86efb
川野勇也 <kawanoisamunari@kawanoisamunarinoMacBook-Pro.local>
2022-12-03 05:07:51 +0900

    フロントエンドとバックエンド連携

--- go/main.go
+++ go/main.go
@@ -3,20 +3,9 @@ package main
 import (
 	"gin-test/go/database"
 	"gin-test/go/routes"
-	"log"
-	"net/http"
-	"text/template"
 
 	_ "github.com/go-sql-driver/mysql"
 )
-
-func HandlerItemForm(w http.ResponseWriter , r *http.Request){
-	tpl := template.Must(template.ParseFiles("frontend"))
-	values := map[string]string{}
-	if err := tpl.ExecuteTemplate(w, "post.html" , values); err != nil {
-		log.Fatal(err)
-	}
-}
 
 func main() {
 	database.ConnectDatabase(1)
@@ -24,7 +13,4 @@ func main() {
 	router := routes.NewRoutes()
 	router.Run()
 
-	http.HandleFunc("/post.html",HandlerItemForm)
-	http.ListenAndServe(":8080",nil)
 }
-
--- go/routes/route.go
+++ go/routes/route.go
@@ -2,6 +2,7 @@ package routes
 
 import (
 	"gin-test/go/handlers"
+	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
@@ -12,5 +13,11 @@ func NewRoutes() *gin.Engine {
 	router.GET("/items", handlers.ListItemsHandler)
 	router.PUT("/items/:id", handlers.UpdateItemHandler)
 	router.DELETE("/items/:id", handlers.DeleteItemHandler)
+
+	router.LoadHTMLGlob("templates/index.html")
+	router.GET("/", func(ctx *gin.Context) {
+		ctx.HTML(http.StatusOK, "index.html", gin.H{})
+	})
+
 	return router
 }
